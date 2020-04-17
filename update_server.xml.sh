c17d794d8951644e51ce2fd707fabf73caf4ad55
harish111992 <63441336+harish111992@users.noreply.github.com>
2020-04-17 16:56:24 +0530

    Server.xml update_ new deplyment

---
+++ update_server.xml.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+Date=`date +%Y-%m-%d`
+for i in `seq 1 10`;
+do
+/bin/cp -rp /usr/tomcat.$i/conf/server.xml /usr/tomcat.$i/bin/server.xml$Date
+echo "Backup complete for /usr/tomcat.$i/conf/server.xml"
+echo "updating /usr/tomcat.$i/conf/server.xml"
+## /tmp/content is the file, having data which need to be append on server.xml after the pattern <GlobalNamingResources>
+sed -i '/<GlobalNamingResources>/r /tmp/content /usr/tomcat.$i/conf/server.xml
+echo 'updated server.xml file on tomcat.$i'
+done
+####-----------TESTED---------####
